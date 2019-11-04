27b03fa8863d974917af55213d2172fd3ef69d48
Riley <rileyschoppa@yahoo.com>
2019-11-04 10:41:02 -0500

    webhook routes, methods, and forms all working

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,19 +1,86 @@
 class WebhookController < ApplicationController
   def new
+    @user = User.find(session[:user_id])
+    if @user == nil
+      redirect_to '/user/login'
+    end
+
+
   end
 
   def create
+    @user = User.find(session[:user_id])
+    if @user == nil
+      redirect_to '/user/login'
+    end
+
+    @webhook = Webhook.new(params.permit(:name, :url))
+
+    @webhook.user = @user
+
+    if @webhook.save
+      redirect_to '/webhook/list'
+    else
+      render 'new'
+    end
+
   end
 
   def edit
+    @user = User.find(session[:user_id])
+    if @user == nil
+      redirect_to '/user/login'
+    end
+
+    @webhook = Webhook.find(params[:id])
+
   end
 
   def update
+    @user = User.find(session[:user_id])
+    if @user == nil
+      redirect_to '/user/login'
+    end
+
+    @webhook = Webhook.find(params[:id])
+    @webhook.name = params[:name]
+    @webhook.url = params[:url]
+
+    if @webhook.save
+      redirect_to '/webhook/list'
+    else
+      redirect_to "/webhook/#{@webhook.id}/edit"
+    end
+
   end
 
   def delete
+    @user = User.find(session[:user_id])
+    if @user == nil
+      redirect_to '/user/login'
+    end
+
+    @webhook = Webhook.find(params[:id])
+
+    if @webhook.user.id == session[:user_id]
+      @webhook.destroy
+      redirect_to '/webhook/list'
+    else
+      redirect_to '/webhook/list'
+    end
+
   end
 
   def list
+    @user = User.find(session[:user_id])
+    if @user == nil
+      redirect_to '/user/login'
+    end
+
+    @webhooks = Webhook.where(user_id: @user.id)
+
   end
 end
+
+
+#https://discordapp.com/api/webhooks/640932609915879450/rmYF40JRT4XOld720KimgYBrHZxm49i2xsLNo_UqtQ5XtJArJAHs8OvNo6RObP6xaI48
